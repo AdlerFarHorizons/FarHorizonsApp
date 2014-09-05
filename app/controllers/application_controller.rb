8060862232b2362dcdab74c691b7a2b62fa1982b
lnigra <lounigra@gmail.com>
2014-09-05 09:48:47 -0500

    Comments on alt forgery protection.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  # 
+  # Suggested at https://coderwall.com/p/8z7z3a Might try this:
+  # protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format.json? }
+  
   protect_from_forgery with: :null_session
 end
