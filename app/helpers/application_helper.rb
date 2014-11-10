06891339a245097a704602334558aa477ae97f0b
lnigra <lounigra@gmail.com>
2014-11-10 08:17:43 -0600

    Include pseudo-associations (model keys with _id) in expansion.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,15 +3,37 @@ module ApplicationHelper
   def assoc_tree( obj )
     # Create level 1 tree
     result = []
-    obj.associations.keys.each do |x|
+    assocs = obj.associations.keys
+    # Add pseudo-associations: identified by key ending in '_id(s)'
+    # but exclude '_id' itself and the parent id if there's a 'belongs to'
+    # association.
+    obj.keys.keys.select { |x| 
+      !x.eql?( '_id' ) && 
+      !x.eql?( x[1].underscore + '_id' ) && 
+      x.singularize.end_with?( '_id' ) }.each {|x| assocs << x.to_sym}
+    assocs.each do |x|
       parent_class = obj.class.to_s
       parent_id = obj.id.to_s
-      assoc_klass = x.to_s.singularize.titleize.split.join
-      assoc_objs = eval( 'obj.' + x.to_s )
-      assoc_type = assoc_objs.association.to_s.split(":")[6].split('Association')[0]
+      is_pseudo = x.to_s.singularize.end_with?( '_id' )
+      klass_key = is_pseudo ? 
+                  x.to_s.singularize.rpartition( '_id' )[0].to_sym : x
+      assoc_klass = klass_key.to_s.singularize.titleize.split.join
+      if is_pseudo
+        ids = eval( 'obj.' + x.to_s )
+        assoc_objs = eval( assoc_klass + '.find( ids )' ) 
+      else        
+        assoc_objs = eval( 'obj.' + x.to_s )
+      end
+      if is_pseudo
+        assoc_type = x.to_s.end_with?( 's' ) ? 'Many' : 'One'
+      else
+        assoc_type = 
+          assoc_objs.association.to_s.split(":")[6].split('Association')[0]
+      end
       has_objs = false # Flag used to allow at least a nil placeholder
       unless assoc_objs.class == Array
-        result << [1, parent_class, parent_id, assoc_type, assoc_objs.class.to_s, assoc_objs ]
+        result << [1, parent_class, parent_id, assoc_type, 
+                   assoc_objs.class.to_s, assoc_objs ]
         has_objs = true
       else
         assoc_objs.each do |y|
@@ -19,35 +41,73 @@ module ApplicationHelper
           has_objs = true
         end
       end
-      result << [1, parent_class, parent_id, assoc_type, assoc_klass, nil] unless has_objs
+      result << [1, parent_class, parent_id, assoc_type, assoc_klass, 
+                 nil] unless has_objs
     end
-    result.each do |r| 
+    result.each do |r|
+      puts r.to_s
     end
+    
+    # Drill down
     result.each_with_index do |x, i|
       insert = i + 1
       level = x[0]
       klass = x[4]
       object = x[5]
-      eval(klass).associations.keys.each_with_index do |y,j|
-        assoc = eval("#{klass}.associations[:#{y.to_s}]").to_s.split(":")[6].split('Association')[0]
-        sub_klass = y.to_s.singularize.titleize.split.join
+      assocs = eval(klass).associations.keys
+      # Add pseudo-associations: identified by key ending in '_id(s)'
+      # but exclude '_id' itself and the parent id if there's a 'belongs to'
+      # association.
+      eval(klass).keys.keys.select { |k| 
+        !k.eql?( '_id' ) && 
+        !k.eql?( x[1].underscore + '_id' ) && 
+        k.singularize.end_with?( '_id' ) }.each { |kk| assocs << kk.to_sym}
+      assocs.each_with_index do |y,j|
+        is_pseudo = y.to_s.singularize.end_with?( '_id' )
+        klass_key = is_pseudo ? 
+          y.to_s.singularize.rpartition( '_id' )[0].to_sym : y
+        if is_pseudo
+          assoc = y.to_s.end_with?( 's' ) ? 'Many' : 'One'
+        else
+          assoc = eval("#{klass}.associations[:#{y.to_s}]")
+                        .to_s.split(":")[6].split('Association')[0]
+        end
+        sub_klass = klass_key.to_s.singularize.titleize.split.join
+        puts "klass:#{klass} klass_key:#{klass_key} object:#{object}"
         parent_id = object ? object.id.to_s : nil
         unless eval(sub_klass).keys['no_edit']
           if ( assoc == 'One' )
-            assoc_objs = object ? eval( 'object.' + y.to_s ) : nil
-            result.insert( insert, [ level + 1, klass, parent_id, assoc, sub_klass, assoc_objs ] )
+            if is_pseudo
+              ids = eval( 'object.' + y.to_s )
+              assoc_objs = eval( sub_klass + '.find( ids )' )
+            else
+              assoc_objs = object ? eval( 'object.' + y.to_s ) : nil
+            end
+            result.insert( 
+              insert, 
+              [ level + 1, klass, parent_id, assoc, sub_klass, assoc_objs ] )
             insert += 1
           end
           if assoc == 'Many'
-            assoc_objs = object ? eval( 'object.' + y.to_s ) : []
+            if is_pseudo
+              ids = eval( 'object.' + y.to_s )
+              assoc_objs = eval( sub_klass + '.find( ids )' )
+            else
+              assoc_objs = object ? eval( 'object.' + y.to_s ) : []
+            end
             parent_id = object ? object.id.to_s : nil
             if assoc_objs.count > 0 
               assoc_objs.each_with_index do |z,k|
-                result.insert( insert, [ level + 1, klass, parent_id, assoc, y.to_s.singularize.titleize.split.join, z ] )
+                result.insert( 
+                  insert, 
+                  [ level + 1, klass, parent_id, 
+                    assoc, y.to_s.singularize.titleize.split.join, z ] )
                 insert += 1
               end
             else
-              result.insert( insert, [ level + 1, klass, parent_id, assoc, y.to_s.singularize.titleize.split.join, nil ] )
+              result.insert( insert, 
+                [ level + 1, klass, parent_id, 
+                  assoc, y.to_s.singularize.titleize.split.join, nil ] )
               insert += 1
             end
           end
