2f0f98d7d842a630e8c6600d32a6911a2699a592
lnigra <lounigra@gmail.com>
2015-01-02 08:58:31 -0600

    added surface coords, xyz defined as ECEF, added defaults and :handle key.

--- app/models/point.rb
+++ app/models/point.rb
@@ -5,14 +5,24 @@ class Point
   include MongoMapper::Document
 
   key :time, Time, :default => Time.at(0) # Timestamp
+
+  # ECEF
   key :x, Float, :default => 0.0  
   key :y, Float, :default => 0.0
   key :z, Float, :default => 0.0
-  key :vg, Float, :default => 0.0  
-  key :vx, Float
-  key :vy, Float
-  key :vz, Float
+  key :vx, Float, :default => 0.0
+  key :vy, Float, :default => 0.0
+  key :vz, Float, :default => 0.0
+  
+  # Surface-based
+  key :lat, Float, :default => 0.0
+  key :lon, Float, :default => 0.0
+  key :alt, Float, :default => 0.0
+  key :vg, Float, :default => 0.0 
+  key :heading, Float, :default => 0.0
+  
   key :source_sn, String # LocationDevice or Beacon serial_no
+  key :handle, String # APRS call sign, beacon ID or vehicle handle
   key :no_edit, Boolean, :default => true
 
   timestamps!
