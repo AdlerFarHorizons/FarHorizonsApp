09fe5cc06405db402c38d41a0983986360c083a0
Lou Nigra <lounigra@gmail.com>
2015-02-10 08:55:07 -0600

    Reciprocate possible has_many association

--- app/models/point.rb
+++ app/models/point.rb
@@ -24,6 +24,12 @@ class Point
   key :id_source, String # LocationDevice or Beacon id
   key :ident, String # beacon ident or vehicle ident 
   key :no_edit, Boolean, :default => true
+  
+  # Double belongs_to is lame but a result of point being a common belongs_to
+  # for sky_track and ground_track. They both need to have their has_many points
+  # association reciprocated. In practice one of these will return nil.
+  belongs_to :ground_track
+  belongs_to :sky_track
 
   timestamps!
   
