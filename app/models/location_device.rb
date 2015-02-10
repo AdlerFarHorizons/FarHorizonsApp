a212ae0787c0aa3e90e5ecbf55ec251562d0125d
Lou Nigra <lounigra@gmail.com>
2015-02-10 00:25:37 -0600

    Remove point from location_device

--- app/models/location_device.rb
+++ app/models/location_device.rb
@@ -8,20 +8,7 @@ class LocationDevice
   key :driver_pid, Integer
   key :port, String
   key :persistent, Boolean, :default => true
-    
-  has_one :point #this point is persistent. It's fields are updated on each fix
-  
+      
   timestamps!
   
-  after_create :initialize_point
-
-  
-  private
-
-  def initialize_point
-    self.point = Point.create( :source_sn => self.serial_no )
-    self.point.save
-    self.save
-  end
-
 end
