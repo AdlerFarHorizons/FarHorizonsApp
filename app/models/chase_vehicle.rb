1b06c86876b338109cbdb861262d815436cea827
Lou Nigra <lounigra@gmail.com>
2015-03-30 07:26:29 -0500

    Methods for pseudo-associations

--- app/models/chase_vehicle.rb
+++ app/models/chase_vehicle.rb
@@ -10,5 +10,11 @@ class ChaseVehicle
   has_one :ground_track
   
   timestamps!
+  
+  # Methods for pseudo-associations
+  
+  def chase_server
+    ChaseServer.find( chase_server_id )
+  end
 
 end
