749128eac30b50d8888445ffb9933102a278ca4c
lnigra <lounigra@gmail.com>
2015-07-13 02:06:53 -0500

    Special route to enable controls

--- app/controllers/tracker_controller.rb
+++ app/controllers/tracker_controller.rb
@@ -2,5 +2,10 @@ class TrackerController < ApplicationController
   
   def index
   end
+  
+  def login
+    @admin = ( params[:passwd] == '33kmisasgoodasspace')
+    render 'tracker/index'
+  end
 
 end
