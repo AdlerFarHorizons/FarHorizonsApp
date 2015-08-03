dee7e64a0f98f5edea4764f38b8609f84c3c29f9
lnigra <lounigra@gmail.com>
2015-08-03 09:57:29 -0500

    Configuration fixes for Rails 4 production

---
+++ config/initializers/assets.rb
@@ -0,0 +1,9 @@
+Rails.application.config.assets.precompile += %w( 
+                                                  tracking_home/tracking_home.js 
+                                                  tracking_home/chatbox2.js
+                                                  tracking_home/testChart.js
+                                                  tracker_new/tracker_new.js 
+                                                  tracker_new/chatbox2.js
+                                                  tracker_new/testChart.js
+                                                  tracker.js
+                                                )
