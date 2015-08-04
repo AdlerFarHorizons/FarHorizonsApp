e58151bdb79c3c86e1faa6d833a2bce3909f340b
lnigra <lounigra@gmail.com>
2015-08-04 01:31:45 -0500

    Make pusher config portable.

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -5,7 +5,7 @@ config = YAML.load_file(Rails.root + 'config' + 'pusher.yml')[Rails.env]
 Pusher.app_id = config['app_id']
 Pusher.key    = config['key']
 Pusher.secret = config['secret']
-Pusher.host = 'yudisthira'
+Pusher.host = Socket.gethostname()
 Pusher.port = 4567
 #Pusher.url = 
 #  "https://#{Pusher.key}:#{Pusher.secret}@api.pusherapp.com/apps/#{Pusher.app_id}"
